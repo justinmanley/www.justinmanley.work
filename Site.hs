0f98f30a56863ea0f3979fc68694a32fc7215b80
Justin Manley <manleyjster@gmail.com>
2019-04-16 09:30:04 -0700

    Add archive page for writing.

--- Site.hs
+++ Site.hs
@@ -9,6 +9,8 @@ import Data.Text (Text)
 import Data.Typeable
 import Hakyll
 
+import Debug.Trace (trace)
+
 import OutOfTheYards.Content.Normalize (normalizeUrls)
 -----------------------------------------------------------------------------
 main :: IO ()
@@ -34,33 +36,55 @@ main = hakyll $ do
     match "templates/*" $ compile templateCompiler
 
     -- Metadata for posts published elsewhere on the internet.
-    matchMetadata "posts/**/*.md" hasSource $
-        -- Posts which have a 'source' field are published somewhere else on the internet.
-        -- As a result, they typically do not have a 'body', and should not be included in
-        -- the archive of posts published on this website.
-        compile $ pandocCompiler
+    matchMetadata "posts/**/*.md" hasSource $ do
+        -- Posts which have a 'source' field are published somewhere else on the
+        -- internet. As a result, they typically do not have a 'body', and
+        -- should not be published as standalone posts on this website. Omitting
+        -- a `route` statement from this `matchMetadata` block ensures that
+        -- these posts do not produce standalone HTML output.
+        compile $ postPreviewCompiler
 
     -- Full content and metadata of posts published on this website.
     matchMetadata "posts/**/*.md" (not . hasSource) $ do
         route $ setExtension "html"
-        compile $ pandocCompiler
-            >>= saveSnapshot "post-body"
-            >>= loadAndApplyTemplate "templates/post.html"    postCtx
-            >>= normalizeUrls postCtx
-            >>= saveSnapshot "post-full"
-            >>= loadAndApplyTemplate "templates/default.html" postCtx
-            >>= relativizeUrls
+        compile $ do
+            postPreviewCompiler
+
+            -- This must be the last statement in the do-block, since it is
+            -- meant to produce the "final" state of each post (the full
+            -- HTML for the standalone post).
+            pandocCompiler
+                >>= saveSnapshot "post-body"
+                >>= loadAndApplyTemplate "templates/post.html" postCtx
+                >>= normalizeUrls postCtx
+                >>= saveSnapshot "post-full"
+                >>= loadAndApplyTemplate "templates/default.html" postCtx
+                >>= relativizeUrls
 
     create ["index.html"] $ do
         route idRoute
         compile $ do
-            let featuredPosts = recentFirst =<< loadAll "posts/**/*.md"
-            let archiveCtx =
-                    listField "posts" postCtx featuredPosts `mappend`
+            posts1 <- recentFirst =<< loadAllSnapshots "posts/**/*.md" "post-preview"
+            let posts = trace ("posts: " ++ show posts1) posts1
+            let homeCtx =
+                    listField "posts" postCtx (return posts) `mappend`
                     defaultContext
 
             makeItem ""
-                >>= loadAndApplyTemplate "templates/home.html" archiveCtx
+                >>= loadAndApplyTemplate "templates/home.html" homeCtx
+                >>= loadAndApplyTemplate "templates/default.html" homeCtx
+                >>= relativizeUrls
+
+    create ["writing/index.html"] $ do
+        route idRoute
+        compile $ do
+            let posts = recentFirst =<< loadAllSnapshots "posts/**/*.md" "post-preview"
+            let archiveCtx =
+                    listField "posts" postCtx posts `mappend`
+                    defaultContext
+
+            makeItem ""
+                >>= loadAndApplyTemplate "templates/writing.html" archiveCtx
                 >>= loadAndApplyTemplate "templates/default.html" archiveCtx
                 >>= relativizeUrls
 
@@ -74,11 +98,19 @@ main = hakyll $ do
             renderAtom feedConfiguration feedCtx posts
 
 -----------------------------------------------------------------------------
+
+-- Helpers
+
 hasMetadata :: Text -> Metadata -> Bool
 hasMetadata key = isJust . HashMap.lookup key
 
 hasSource :: Metadata -> Bool
 hasSource = hasMetadata "source"
+
+postPreviewCompiler :: Compiler (Item String)
+postPreviewCompiler = pandocCompiler
+    >>= loadAndApplyTemplate "templates/post_preview.html" postCtx
+    >>= saveSnapshot "post-preview"
 
 loadAllSnapshotsMatchingMetadata :: (Binary a, Typeable a) => Pattern -> Snapshot -> (Metadata -> Bool) -> Compiler [Item a]
 loadAllSnapshotsMatchingMetadata pattern snapshot metadataPred = do
@@ -88,6 +120,9 @@ loadAllSnapshotsMatchingMetadata pattern snapshot metadataPred = do
 postCtx =
     dateField "date" "%B %e, %Y" `mappend`
     defaultContext
+
+
+-- Configuration
 
 feedConfiguration :: FeedConfiguration
 feedConfiguration = FeedConfiguration
