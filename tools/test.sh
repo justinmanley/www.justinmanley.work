f1e200e57ec4cd4a4f62ab18b28967223cd06f2e
Justin Manley <manleyjster@gmail.com>
2019-05-02 19:15:46 -0700

    Move stylelint from site-build to the pre-commit hook.

--- tools/test.sh
+++ tools/test.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
 
-stack test
+TEST_STATUS=0
+
+npx stylelint css/*.scss || TEST_STATUS=$?
+
+stack test || TEST_STATUS=$?
+
+# Return the exit code of the last failing command.
+exit $TEST_STATUS
