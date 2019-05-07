cfc334f2e4476e694d88883a419690ca43ea13f5
Justin Manley <manleyjster@gmail.com>
2019-05-07 08:55:32 -0700

    Add percy screenshot capture to pre-commit hook.

--- tools/test.sh
+++ tools/test.sh
@@ -4,6 +4,11 @@ TEST_STATUS=0
 
 npx stylelint css/*.scss || TEST_STATUS=$?
 
+# Must `export PERCY_TOKEN` on the command-line before running
+# this command. The PERCY_TOKEN is located at
+# https://percy.io/justinmanley/www.justinmanley.work/settings.
+percy snapshot _site || TEST_STATUS=$?
+
 stack test || TEST_STATUS=$?
 
 # Return the exit code of the last failing command.
